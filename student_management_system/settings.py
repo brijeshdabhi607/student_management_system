ca397d70d9061821edd4f110c976fb06eff21106
brijesh <john@gmail.com>
2023-12-24 16:56:56 +0530

    static files and media fiels configrations

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'app'  # app is the name of the app we created
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# static file url configrations
+
+STATIC_URL = 'static/' 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# media file configrations
+
+MEDIA_URL = '/media/'
+
+MEDIA_DIRS = [
+    os.path.join(BASE_DIR, 'media')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
