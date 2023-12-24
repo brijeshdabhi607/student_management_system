bbcb96165c39fb803de098c75c746ec9da3d29f9
brijesh <john@gmail.com>
2023-12-24 17:35:48 +0530

    created url and views

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -125,7 +125,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # media file configrations
 
@@ -139,3 +138,9 @@ MEDIA_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# we created a custom user model in app/models.py
+AUTH_USER_MODEL = 'app.CustomUser'
+
+# Registering Custom Backend "EmailBackEnd"
+AUTHENTICATION_BACKENDS = ['app.EmailBackEnd.EmailBackEnd']
